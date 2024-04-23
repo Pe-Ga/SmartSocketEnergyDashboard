62455334049e8771dec35a76944981c6fd7beb54
Peter Gamsjäger <if21b016@technikum-wien.at>
2024-04-23 15:20:37 +0200

    delete: clean unused fields and methods in controller

--- backend/SmartSocketEnergyDashboard/src/main/java/at/technikum/SmartSocketEnergyDashboard/controllers/HomeController.java
+++ backend/SmartSocketEnergyDashboard/src/main/java/at/technikum/SmartSocketEnergyDashboard/controllers/HomeController.java
@@ -18,20 +18,4 @@ public class HomeController {
     @Value("${smartsocket.device1.ip}")
     private static String deviceIp;
 
-    private static final String url = "http://192.168.0.57/cm?cmnd=EnergyTotal";
-
-
-    // http://localhost:8181/v1/api
-    // Send Request to smartsocket every 5s
-/*
-    @GetMapping("/v1/api")
-    public void getEnergyTotal() {
-        logger.info("Device IP: {}",deviceIp);
-        // Construct the URL with the device IP, port, and command
-        String response = smartSocketService.callTasmotaEndpoint();
-        logger.info(response);
-
-        // TODO implement serialization and save data in DB
-    }
-*/
 }
