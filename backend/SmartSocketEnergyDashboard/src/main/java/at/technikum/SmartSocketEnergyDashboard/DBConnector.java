5a88bcbe2e7ec4892077c3b7504487e8ad31106f
Peter Gamsjäger <if21b016@technikum-wien.at>
2024-04-26 16:26:29 +0200

    delete: unused import

--- backend/SmartSocketEnergyDashboard/src/main/java/at/technikum/SmartSocketEnergyDashboard/DBConnector.java
+++ backend/SmartSocketEnergyDashboard/src/main/java/at/technikum/SmartSocketEnergyDashboard/DBConnector.java
@@ -2,16 +2,11 @@ package at.technikum.SmartSocketEnergyDashboard;
 
 import com.influxdb.client.InfluxDBClient;
 import com.influxdb.client.InfluxDBClientFactory;
-import com.influxdb.client.WriteApiBlocking;
-import com.influxdb.client.domain.WritePrecision;
-import com.influxdb.client.write.Point;
-import com.influxdb.exceptions.InfluxException;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.time.Instant;
 
 @Setter
 @Getter
@@ -37,22 +32,5 @@ public class DBConnector {
         setUrl(url);
         return InfluxDBClientFactory.create(getUrl(), getToken().toCharArray(), getOrg(), getBucket());
     }
-    /*
-        public boolean singlePointWrite(InfluxDBClient influxDBClient) {
-            boolean flag = false;
-            try {
-                WriteApiBlocking writeApi = influxDBClient.getWriteApiBlocking();
 
-                Point point = Point.measurement("sensor").addTag("sensor_id", "TLM0100").addField("location", "Main Lobby")
-                        .addField("model_number", "TLM89092A")
-                        .time(Instant.parse("2021-10-11T15:18:15.117484Z"), WritePrecision.MS);
-
-                writeApi.writePoint(point);
-                flag = true;
-            } catch (InfluxException e) {
-                System.out.println("Exception!!" + e.getMessage());
-            }
-            return flag;
-        }
-    */
 }
--- backend/SmartSocketEnergyDashboard/src/main/java/at/technikum/SmartSocketEnergyDashboard/config/Config.java
+++ backend/SmartSocketEnergyDashboard/src/main/java/at/technikum/SmartSocketEnergyDashboard/config/Config.java
@@ -4,7 +4,6 @@ package at.technikum.SmartSocketEnergyDashboard.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class Config {
