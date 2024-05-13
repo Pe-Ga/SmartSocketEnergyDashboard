3d398404c10f6347fad56295b2c60ef0094e0bcf
Peter Gamsjäger <if21b016@technikum-wien.at>
2024-05-13 21:30:09 +0200

    update: minor changes.

--- frontend/dashboard_frontend/volt_vision_frontend/src/components/MainBody.svelte
+++ frontend/dashboard_frontend/volt_vision_frontend/src/components/MainBody.svelte
@@ -4,11 +4,13 @@
     <main>
         <h2>TASMOTA DASHBOARD</h2>
         <div class="dashboard-container">
-                <iframe src="http://localhost:3000/public-dashboards/a3e605db9b5c4b90875a2b4bc90770d7" title="voltage display" class="frame-one"></iframe>
-                <iframe src="http://localhost:3000/public-dashboards/a3e605db9b5c4b90875a2b4bc90770d7" title="voltage display" class="frame-two"></iframe>
-                <iframe src="http://localhost:3000/public-dashboards/a3e605db9b5c4b90875a2b4bc90770d7" title="voltage display" class="frame-three"></iframe>
-                <iframe src="http://localhost:3000/public-dashboards/a3e605db9b5c4b90875a2b4bc90770d7" title="voltage display" class="frame-four"></iframe>
-                <iframe src="http://localhost:3000/public-dashboards/a3e605db9b5c4b90875a2b4bc90770d7" title="voltage display" class="frame-five"></iframe>
+            <iframe src="http://localhost:3000/d-solo/ddligxgufshs0c/tasmota-voltage-dashboard?orgId=1&from=1715606227568&to=1715627827568&panelId=4" title="energy total" class="frame-one"></iframe>
+            <iframe src="http://localhost:3000/dashboard/new?orgId=1&from=1715604451245&to=1715626051245&viewPanel=1" title="voltage display" class="frame-two"></iframe>
+             <iframe src="http://localhost:3000/d-solo/ddligxgufshs0c/tasmota-voltage-dashboard?orgId=1&from=1715605063922&to=1715626663922&panelId=2" title="current" class="frame-three"></iframe>
+            <iframe src="http://localhost:3000/d-solo/ddligxgufshs0c/tasmota-voltage-dashboard?orgId=1&from=1715605416240&to=1715627016240&panelId=3" title="voltage display" class="frame-four"></iframe>
+            <iframe src="http://localhost:3000/d-solo/ddligxgufshs0c/tasmota-voltage-dashboard?orgId=1&from=1715605214723&to=1715626814723&panelId=1" title="voltage display" class="frame-five"></iframe>
+            <iframe src="http://localhost:3000/d-solo/ddligxgufshs0c/tasmota-voltage-dashboard?orgId=1&from=1715606067921&to=1715627667921&panelId=4" title="energy today" class="frame-six"></iframe>
+
         </div>
     </main>
 
@@ -26,7 +28,8 @@
         border: none;
     }
 
-    .frame-five {
+    .frame-five, .frame-six{
         grid-column: 1 / -1;
     }
+
 </style>
