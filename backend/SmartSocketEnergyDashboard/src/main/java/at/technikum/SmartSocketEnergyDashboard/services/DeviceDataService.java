a9902b3016c3e6fdf116088a7a20852a7327777c
Peter Gamsjäger <if21b016@technikum-wien.at>
2024-05-17 10:58:00 +0200

    refactor: DeviceDataService.java fetchDeviceStatus return to Optional <>. minor changes in DeviceDataLogger.

--- backend/SmartSocketEnergyDashboard/src/main/java/at/technikum/SmartSocketEnergyDashboard/services/DeviceDataService.java
+++ backend/SmartSocketEnergyDashboard/src/main/java/at/technikum/SmartSocketEnergyDashboard/services/DeviceDataService.java
@@ -6,8 +6,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Optional;
 
 @Service
 public class DeviceDataService {
@@ -24,14 +27,18 @@ public class DeviceDataService {
         this.objectMapper = objectMapper;
     }
 
-    public DeviceDTO fetchDeviceData(String ipAddress) {
+    public Optional<DeviceDTO> fetchDeviceStatus(String ipAddress) {
         String endpointUrl = buildEndpointUrl(ipAddress, DEVICE_COMMAND);
         try {
             String response = restTemplate.getForObject(endpointUrl, String.class);
-            return objectMapper.readValue(response, DeviceDTO.class);
+            DeviceDTO deviceDTO = objectMapper.readValue(response, DeviceDTO.class);
+            return Optional.of(deviceDTO);
         } catch (JsonProcessingException e) {
             logger.error("Failed to parse JSON response from device at IP: {}", ipAddress, e);
             throw new RuntimeException("Error parsing device data", e);
+        } catch (ResourceAccessException e) {
+            logger.warn("Device at IP: {} is not reachable", ipAddress);
+            return Optional.empty();  // Return an empty Optional if the device is not reachable
         } catch (RestClientException e) {
             logger.error("HTTP request failed for device at IP: {}", ipAddress, e);
             throw new RuntimeException("Error communicating with device", e);
@@ -39,7 +46,6 @@ public class DeviceDataService {
     }
 
     public String buildEndpointUrl(String ipAddresse, String command) {
-        // http://192.168.0.57/cm?cmnd=status0
         logger.info(String.format("Send Command: %s to IP: %s",command, ipAddresse));
         return String.format("http://%s/cm?cmnd=%S",ipAddresse, command);
     }
